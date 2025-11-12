c6b3fabac901443dd888fc0bb7d975b8aaf1a448
ali-ctf-player <eliekberbabayev23082007@gmail.com>
2025-11-12 12:24:15 +0400

    Python

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
 def islower(c):
-    for i in range(97, 123):
-        if "{:c}".format(i) == c:
-            return True
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
