3ffa0a8b1681b7bf79af8ee370b000faf77bdead
ali-ctf-player <eliekberbabayev23082007@gmail.com>
2025-11-12 18:27:17 +0400

    Python

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@
 def uppercase(a):
     result = ""
     for i in a:
-        if ord(i) >= 97 and i <= 122:
+        if ord(i) >= 97 and ord(i) <= 122:
             result += chr(ord(i) - 32)
         else:
             result += i
