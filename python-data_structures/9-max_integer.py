2ea157ffd8182f415318fb5315bd613e97cf09a4
ali-ctf-player <eliekberbabayev23082007@gmail.com>
2025-11-17 21:23:23 +0400

    quiz commit

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
 def max_integer(my_list):
-    result = my_list[0]
+    result = 0
     if len(my_list) == 0:
         result = None
     else:
+        result = my_list[0]
         for i in my_list:
             if i > result:
                 result = i
