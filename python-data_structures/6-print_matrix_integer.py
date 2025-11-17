305217d761695f1ceb8c819b988bd05182100862
ali-ctf-player <eliekberbabayev23082007@gmail.com>
2025-11-17 17:30:06 +0400

    quiz commit

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,17 +3,8 @@
 def print_matrix_integer(matrix=[[]]):
     for x in matrix:
         for y in range(len(x)):
-            if y + 1  == len(x):
+            if y + 1 == len(x):
                 print("{:d}".format(x[y]), end="")
             else:
                 print("{:d}".format(x[y]), end=" ")
         print()
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-print_matrix_integer(matrix)
-print("--")
-print_matrix_integer()
