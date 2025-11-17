7b27673015cd6e7119c21721cacb38813c1d3c6a
ali-ctf-player <eliekberbabayev23082007@gmail.com>
2025-11-17 16:21:38 +0400

    quiz commit

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
 
 def replace_in_list(my_list, idx, new_element):
-    if idx >= len(my_list):
+    if idx >= len(my_list) or idx < 0:
         return my_list
     else:
-        if idx < 0:
-            return my_list
-        else:
-            my_list[idx] = new_element
-            return my_list
-
-print(replace_in_list([1,2,3],0,2))
+        my_list[idx] = new_element
+        return my_list
