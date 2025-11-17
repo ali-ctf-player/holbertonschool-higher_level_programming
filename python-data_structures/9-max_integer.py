54092eba6cec0beeeb32e9c75f6fa55d6d003e93
ali-ctf-player <eliekberbabayev23082007@gmail.com>
2025-11-17 21:21:55 +0400

    quiz commit

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def max_integer(my_list):
+    result = 0
+    if len(my_list) == 0:
+        result = None
+    else:
+        for i in my_list:
+            if i > result:
+                result = i
+    return result
