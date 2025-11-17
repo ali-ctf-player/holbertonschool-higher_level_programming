8a15ff92a1126e861ca201a416814a9f1e4381c5
ali-ctf-player <eliekberbabayev23082007@gmail.com>
2025-11-17 11:34:03 +0400

    Module

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python3
-import sys
-
+from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
-    if (len(sys.argv)) == 4:
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
-        if not (sys.argv[2] in ("+", "-", "*", "/")):
-            print("Unknown operator. Available operators: +, -, * and /")
-        else:
-            if sys.argv[2] == "+":
-                print("{} + {} = {}".format(a, b, add(a, b)))
-            if sys.argv[2] == "-":
-                print("{} - {} = {}".format(a, b, sub(a, b)))
-            if sys.argv[2] == "*":
-                print("{} * {} = {}".format(a, b, mul(a, b)))
-            if sys.argv[2] == "/":
-                print("{} / {} = {}".format(a, b, div(a, b)))
-    else:
+    import sys
+    
+    if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
+        sys.exit(1)
+    
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
+    
+    if operator == '+':
+        result = add(a, b)
+    elif operator == '-':
+        result = sub(a, b)
+    elif operator == '*':
+        result = mul(a, b)
+    elif operator == '/':
+        result = div(a, b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    
+    print("{} {} {} = {}".format(a, operator, b, result))
