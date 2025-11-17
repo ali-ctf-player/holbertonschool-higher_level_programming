418ecf8ec0d2de9230772a883d65f473f9f7e012
ali-ctf-player <eliekberbabayev23082007@gmail.com>
2025-11-17 21:12:11 +0400

    quiz commit

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    length = len(sentence)
-    first = sentence[0]
+    if len(sentence) == 0:
+        length = 0
+        first = None
+    else:
+        length = len(sentence)
+        first = sentence[0]
     return length, first
