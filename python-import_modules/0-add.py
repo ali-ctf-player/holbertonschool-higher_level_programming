435c85d988019bf6245ec5905088f5a8662b5c5f
ali-ctf-player <eliekberbabayev23082007@gmail.com>
2025-11-17 09:28:02 +0400

    Module

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 def add(a, b):
+    return a + b
+
+if __name__ == "__main__":
     a = 1
     b = 2
-    return f"{a} + {b} = {a + b}"
-
+    print("{} + {} = {}".format(a, b, add(a, b)))
