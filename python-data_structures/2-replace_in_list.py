ab1a32f53f83ae2086c399ce9691e0238b1dc26a
ali-ctf-player <eliekberbabayev23082007@gmail.com>
2025-11-17 16:20:28 +0400

    quiz commit

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 
 def replace_in_list(my_list, idx, new_element):
-    if idx > len(my_list):
-        return None
+    if idx >= len(my_list):
+        return my_list
     else:
         if idx < 0:
             return my_list
         else:
             my_list[idx] = new_element
             return my_list
+
+print(replace_in_list([1,2,3],0,2))
