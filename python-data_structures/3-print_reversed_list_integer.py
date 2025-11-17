cbd6167ab17cf27bddd1f9dbaf2e5d5925a15790
ali-ctf-player <eliekberbabayev23082007@gmail.com>
2025-11-17 16:51:51 +0400

    quiz commit

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    for i in range(1,len(my_list) + 1):
-        print("{:d}".format(my_list[-i]))
-print_reversed_list_integer([1,2,3])
+    rev_list = []
+    for i in range(1, len(my_list) + 1):
+        rev_list.append(my_list[-i])
+    for i in rev_list:
+        print(i)
