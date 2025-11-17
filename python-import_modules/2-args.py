3072602fa45e28d0ecf746f0ea205a83b03c8a2b
ali-ctf-player <eliekberbabayev23082007@gmail.com>
2025-11-17 10:22:03 +0400

    Module

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 import sys
 
-if len(sys.argv) > 1:
-    if len(sys.argv) == 2:
-        print("{} argument:".format(len(sys.argv) - 1))
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        if len(sys.argv) == 2:
+            print("{} argument:".format(len(sys.argv) - 1))
+        else:
+            print("{} arguments:".format(len(sys.argv) - 1))
+        for i in range(1, len(sys.argv)):
+            print("{}: {}".format(i, sys.argv[i]))
     else:
-        print("{} arguments:".format(len(sys.argv) - 1))
-    for i in range(1, len(sys.argv)):
-        print("{}: {}".format(i, sys.argv[i]))
-else:
-    print("0 arguments.")
+        print("0 arguments.")
+
+
+    
