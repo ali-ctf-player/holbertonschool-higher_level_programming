c01ef3552eaacbe5292203a97c5463df3118ce03
ali-ctf-player <eliekberbabayev23082007@gmail.com>
2025-11-17 10:19:37 +0400

    Module

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+if len(sys.argv) > 1:
+    if len(sys.argv) == 2:
+        print("{} argument:".format(len(sys.argv) - 1))
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
+    for i in range(1, len(sys.argv)):
+        print("{}: {}".format(i, sys.argv[i]))
+else:
+    print("0 arguments.")
