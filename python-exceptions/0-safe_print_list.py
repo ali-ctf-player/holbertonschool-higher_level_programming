d6009bcdd99cb0f8bf4090273d56ff5a52347522
ali-ctf-player <eliekberbabayev23082007@gmail.com>
2025-11-18 22:34:09 +0400

    quiz commit

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -6,6 +6,17 @@ def safe_print_list(mylist, x=0):
         for i in range(x):
             print(mylist[i], end="")
             count += 1
+        print()
     except IndexError:
+        print()
         pass
     return count
+
+my_list = [1, 2, 3, 4, 5]
+
+nb_print = safe_print_list(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
