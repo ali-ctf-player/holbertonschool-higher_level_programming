85367ab68d439b315adb86b72301e8d4ac90264d
ali-ctf-player <eliekberbabayev23082007@gmail.com>
2025-11-18 23:22:33 +0400

    quiz commit

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -5,6 +5,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except Exception:
-        print("Exception: {}".format(value), file=sys.stderr)
+    except (ValueError, TypeError) as e:
+        print("Exception: {}".format(e), file=sys.stderr)
         return False
