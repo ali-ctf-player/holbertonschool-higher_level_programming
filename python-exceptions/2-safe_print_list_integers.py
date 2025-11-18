095520b6eb1a35ab70cbbe35fc04042e8c78f78d
ali-ctf-player <eliekberbabayev23082007@gmail.com>
2025-11-18 22:47:55 +0400

    quiz commit

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(mylist, x=0):
+    count = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(mylist[i]), end="")
+            count += 1
+        except (ValueError, TypeError):
+            continue
+        except IndexError:
+            raise
+    print()
+    return count
