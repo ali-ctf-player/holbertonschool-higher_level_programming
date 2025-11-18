4d60652b8f70a382cb934c45cc39dd56c06c4fb5
ali-ctf-player <eliekberbabayev23082007@gmail.com>
2025-11-18 11:38:10 +0400

    quiz commit

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 
 def weight_average(my_list):
-    prime = 0
-    counter = 0
-    for mytuple_ in my_list:
-        temp = 1
-        for val in mytuple_:
-            temp *= val
-        prime += temp
-        counter += mytuple_[1]
-    return prime / counter
+    if my_list:
+        return 0
+    else:
+        prime = 0
+        counter = 0
+        for mytuple_ in my_list:
+            temp = 1
+            for val in mytuple_:
+                temp *= val
+            prime += temp
+            counter += mytuple_[1]
+        return prime / counter
