96b376869a048746936ab66c6a87b90471dfd9bd
ali-ctf-player <eliekberbabayev23082007@gmail.com>
2025-11-18 23:21:26 +0400

    quiz commit

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
 def safe_print_integer_err(value):
+    import sys
     try:
         print("{:d}".format(value))
         return True
     except Exception:
+        print("Exception: {}".format(e), file=sys.stderr)
         return False
