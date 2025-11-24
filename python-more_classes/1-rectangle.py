296f89bd66775b32beb9256f189dc581d438bb3f
ali-ctf-player <eliekberbabayev23082007@gmail.com>
2025-11-24 10:05:50 +0400

    quiz commit

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""It is about classes in Python"""
+
+
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """It is doc string"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        return self.__width
+
+    @property
+    def height(self):
+        """It is doc string"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        return self.__height
