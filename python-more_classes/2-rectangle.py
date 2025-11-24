20d210a2668237e008bff91cc111ca60df7e6abe
ali-ctf-player <eliekberbabayev23082007@gmail.com>
2025-11-24 10:13:32 +0400

    quiz commit

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""It is about classes in Python"""
+
+
+class Rectangle:
+    """Defines a rectangle with width and height attributes"""
+
+    def __init__(self, width=0, height=0):
+        """Initialize rectangle with optional width and height"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Retrieve the width of the rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width of the rectangle with validation"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrieve the height of the rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the height of the rectangle with validation"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def perimeter(self):
+        if self.__width == 0 and self.__height == 0:
+            return 0
+        else:
+            return 2 * (self.__height + self.__width)
+
+    def area(self):
+        return self.__height * self.__width
