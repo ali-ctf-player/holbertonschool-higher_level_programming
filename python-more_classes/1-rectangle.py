95e9ef0dcd18819b97a5e2ca3c9d2a5e3e4f0909
ali-ctf-player <eliekberbabayev23082007@gmail.com>
2025-11-24 10:08:03 +0400

    quiz commit

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -3,33 +3,37 @@
 
 
 class Rectangle:
-    """It is class named Rectangle"""
+    """Defines a rectangle with width and height attributes"""
+    
     def __init__(self, width=0, height=0):
+        """Initialize rectangle with optional width and height"""
         self.width = width
         self.height = height
-
+    
     @property
     def width(self):
-        """It is doc string"""
+        """Retrieve the width of the rectangle"""
         return self.__width
-
+    
     @width.setter
     def width(self, value):
+        """Set the width of the rectangle with validation"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        return self.__width
-
+        self.__width = value
+    
     @property
     def height(self):
-        """It is doc string"""
+        """Retrieve the height of the rectangle"""
         return self.__height
-
+    
     @height.setter
     def height(self, value):
+        """Set the height of the rectangle with validation"""
         if not isinstance(value, int):
-            raise TypeError("width must be an integer")
+            raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("width must be >= 0")
-        return self.__height
+            raise ValueError("height must be >= 0")
+        self.__height = value
