2620069f3256862f3bf40435a8a22887d13f4d52
ali-ctf-player <eliekberbabayev23082007@gmail.com>
2025-11-24 10:08:23 +0400

    quiz commit

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -4,17 +4,17 @@
 
 class Rectangle:
     """Defines a rectangle with width and height attributes"""
-    
+
     def __init__(self, width=0, height=0):
         """Initialize rectangle with optional width and height"""
         self.width = width
         self.height = height
-    
+
     @property
     def width(self):
         """Retrieve the width of the rectangle"""
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         """Set the width of the rectangle with validation"""
@@ -23,12 +23,12 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-    
+
     @property
     def height(self):
         """Retrieve the height of the rectangle"""
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         """Set the height of the rectangle with validation"""
