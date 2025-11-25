88820d169e71281f2705d5ae7406066e8884c877
ali-ctf-player <eliekberbabayev23082007@gmail.com>
2025-11-25 19:19:55 +0400

    quiz commit

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""It is doc string of python file"""
+
+
+def is_kind_of_class(obj, a_class):
+    """It is inside of function"""
+
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
