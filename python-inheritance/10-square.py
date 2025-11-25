6cd7ac54c1a9631a3f268737e470591642937ecf
ali-ctf-player <eliekberbabayev23082007@gmail.com>
2025-11-25 20:53:04 +0400

    quiz commit

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+    Rectangle class that inherits from BaseGeometry
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Rectangle class that inherits from BaseGeometry
+    """
+    def __init__(self, size):
+
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """
+        Rectangle class
+        """
+        return self.__size * self.__size
