22c2c5a16e2e8aef302f038004cf111254c06ffa
ali-ctf-player <eliekberbabayev23082007@gmail.com>
2025-11-25 21:32:07 +0400

    quiz commit

--- python-inheritance/101-add_attribute.py
+++ python-inheritance/101-add_attribute.py
@@ -7,7 +7,5 @@ def add_attribute(obj, attr_name, attr_value):
 
     if hasattr(obj, "__dict__"):
         setattr(obj, attr_name, attr_value)
-    elif hasattr(type(obj), "__slots__" and attr_name in type(obj).__slots__):
-        setattr(obj, attr_name, attr_value)
     else:
         raise TypeError("can't add new attribute")
