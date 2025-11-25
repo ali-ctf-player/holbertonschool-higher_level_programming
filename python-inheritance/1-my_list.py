e3d9635c7a519b120fa7182175d9b143f313c24e
ali-ctf-player <eliekberbabayev23082007@gmail.com>
2025-11-25 11:53:56 +0400

    quiz commit

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""It is doc string of python inheritance"""
+
+
+class MyList(list):
+    """It is inside of MyList class"""
+
+    def print_sorted(self):
+        print(sorted(self))
