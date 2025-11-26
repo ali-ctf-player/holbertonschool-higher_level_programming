32dd2dfeed00af249821a05b843529b75ad70492
ali-ctf-player <eliekberbabayev23082007@gmail.com>
2025-11-26 09:33:11 +0400

    quiz commit

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,4 +5,5 @@
 def write_file(filename="", text=''):
     """It is doc string"""
     with open(filename, 'w', encoding="utf-8") as f:
-        f.write(text)
+        content = str(text)
+        f.write(content)
