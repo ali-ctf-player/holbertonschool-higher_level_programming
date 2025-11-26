7ae79860340dbca9c168da7d16b2760d605ad25c
ali-ctf-player <eliekberbabayev23082007@gmail.com>
2025-11-26 11:13:46 +0400

    quiz commit

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""It is doc string"""
+
+
+def pascal_triangle(n):
+    results = []
+    if n <= 0:
+        return result
+    else:
+        result.append(1)
+        results.append(result)
+        for i in range(n):
+            result = []
+            for x in range(i + 1):
+                if x == 0 or x == i:
+                    result.append(1)
+                else:
+                    value = results[i - 1][x] + results[i - 1][x - 1]
+                    result.append(value)
+            results.append(result)
+
+    return results
