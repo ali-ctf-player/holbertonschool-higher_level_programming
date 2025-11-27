f805061f05747e6dad9477a45197f7943bd43dbb
ali-ctf-player <eliekberbabayev23082007@gmail.com>
2025-11-27 19:16:35 +0400

    quiz commit

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,7 +1,13 @@
--- Full Creation
+-- Creates table second_table if not exists and inserts multiple rows
 CREATE TABLE IF NOT EXISTS second_table (
     id INT,
     name VARCHAR(256),
     score INT
 );
-INSERT INTO second_table(id, name,score) VALUES(1,'John',10), VALUES(2,'Alex',3), VALUES(3,'Bob',14), VALUES(4,'George',8);
+
+-- Insert multiple records
+INSERT INTO second_table (id, name, score) VALUES
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
