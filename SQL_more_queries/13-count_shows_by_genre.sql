bf7d0ec10a16f717ec266517481150299067a1e4
ali-ctf-player <eliekberbabayev23082007@gmail.com>
2025-11-28 11:40:35 +0400

    quiz commit

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,5 @@
+-- Group
+SELECT tv_genres.name as genre, COUNT(tv_show_genres.id) as number_of_shows 
+FROM tv_genres INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id ADD
+GROUP BY tv_genres.name 
+ORDER BY number_of_shows DESC;
