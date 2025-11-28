20e8f7a6ce3c0e7cdc134c533d0dd69407bf3faf
ali-ctf-player <eliekberbabayev23082007@gmail.com>
2025-11-28 10:45:44 +0400

    quiz commit

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,14 +1,14 @@
--- Cities
+-- Create database hbtn_0d_usa if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
--- USE
-USE htbn_0d_usa;
+-- Use the database
+USE hbtn_0d_usa;
 
--- create
+-- Create table cities if it doesn't exist
 CREATE TABLE IF NOT EXISTS cities (
-    id INT AUTO_INCREMENT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
-    PRIMARY KEY(id),
-    Foreign Key (state_id) REFERENCES states(id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (state_id) REFERENCES states(id)
 );
