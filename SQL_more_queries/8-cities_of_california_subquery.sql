7dcb06b73ce2f2d31a9001de66fa94dd3c5e3b23
ali-ctf-player <eliekberbabayev23082007@gmail.com>
2025-11-28 11:12:36 +0400

    quiz commit

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,4 +2,5 @@
 
 SELECT cities.id, cities.name 
 FROM cities,states 
-WHERE cities.state_id = states.id AND states.name = 'California' ORDER BY cities.id ASC;
+WHERE cities.state_id = states.id AND states.name = 'California' 
+ORDER BY cities.id ASC;
