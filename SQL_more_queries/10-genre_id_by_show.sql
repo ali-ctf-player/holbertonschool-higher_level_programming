ae2f6aa3720df1a12810ac1382ee1883569ff573
ali-ctf-player <eliekberbabayev23082007@gmail.com>
2025-11-28 11:23:22 +0400

    quiz commit

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- Genre
-SELECT tv_shows.title , tv_shows_genre.genre_id
-FROM tv_shows INNER JOIN tv_shows_genre
-ON tv_shows.id = tv_shows_genre.show_id
-ORDER BY tv_shows.title ASC, ORDER BY tv_shows_genre.genre_id ASC; 
+SELECT tv_shows.title , tv_shows_genres.genre_id
+FROM tv_shows INNER JOIN tv_shows_genres
+ON tv_shows.id = tv_shows_genres.show_id
+ORDER BY tv_shows.title ASC, ORDER BY tv_shows_genres.genre_id ASC; 
