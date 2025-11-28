00779006413b4c62b0921460d3d8b4768f9a23b1
ali-ctf-player <eliekberbabayev23082007@gmail.com>
2025-11-28 23:01:53 +0400

    quiz commit

--- SQL_more_queries/102-rating_shows.sql
+++ SQL_more_queries/102-rating_shows.sql
@@ -2,7 +2,7 @@
 
 SELECT tv_shows.title,SUM(rate) AS 'rating' 
 FROM tv_shows 
-INNER JOIN tv_show_ratings ON tv_shows.id = tv_shows_ratings.show_id
+INNER JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
 GROUP BY tv_shows.title
 ORDER BY SUM(rate) DESC;
 
