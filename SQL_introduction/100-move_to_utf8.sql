7aaea4e588949ca44ee98b7ffa10e0de87800ef2
ali-ctf-player <eliekberbabayev23082007@gmail.com>
2025-11-28 22:39:01 +0400

    quiz commit

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,9 +1,12 @@
 -- UTFFF
 
+-- Convert database to UTF8
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+-- Use the database
 USE hbtn_0c_0;
 
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Convert the entire table to UTF8
+ALTER TABLE first_table 
+CONVERT TO CHARACTER SET utf8mb4 
+COLLATE utf8mb4_unicode_ci;
