82d77acac5b91dd4dd6cef175331df82e697a77d
ali-ctf-player <eliekberbabayev23082007@gmail.com>
2025-11-28 12:07:00 +0400

    quiz commit

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -3,4 +3,4 @@
 SELECT tv_shows.title , tv_genres.name 
 FROM tv_shows
 LEFT JOIN tv_genres ON tv_shows.id = tv_genres.show_id
-ORDER BY tv_shows.title ASC, ORDER BY tv_genres.name ASC;
+ORDER BY tv_shows.title ASC,tv_genres.name ASC;
