f72b07ed5ff97ee2158b63486b73ff374e5772b7
ali-ctf-player <eliekberbabayev23082007@gmail.com>
2025-11-28 11:59:58 +0400

    quiz commit

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,5 +1,6 @@
 -- Comedy
 SELECT tv_shows.title 
-INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
 FROM tv_shows
-WHERE tv_shows.title = 'Comedy';
+INNER JOIN tv_genres ON tv_shows.id = tv_genres.show_id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY tv_shows.title ASC;
