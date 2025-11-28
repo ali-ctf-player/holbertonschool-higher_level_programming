e66674f693fd6427edfc30ee2ea3c59c28d99319
ali-ctf-player <eliekberbabayev23082007@gmail.com>
2025-11-28 10:44:21 +0400

    quiz commit

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,10 +1,14 @@
 -- Cities
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- USE
 USE htbn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities(
+
+-- create
+CREATE TABLE IF NOT EXISTS cities (
     id INT AUTO_INCREMENT NOT NULL,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
     PRIMARY KEY(id),
     Foreign Key (state_id) REFERENCES states(id)
-)
+);
