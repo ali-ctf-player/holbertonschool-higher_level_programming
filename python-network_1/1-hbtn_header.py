49a09b7e14afbfc9f14a078a2e94afad223ea286
ali-ctf-player <eliekberbabayev23082007@gmail.com>
2025-12-01 09:39:58 +0400

    quiz commit

---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import sys
+import urllib.request
+
+url = urllib.request.Request(sys.argv[1])
+
+with urllib.request.urlopen(url) as page:
+    headers = page.headers
+
+    request_id = headers['X-Request-Id']
+    print(request_id)
