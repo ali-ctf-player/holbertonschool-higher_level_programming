02af186f3d2502518fc4ea12c993d31aba0096fc
ali-ctf-player <eliekberbabayev23082007@gmail.com>
2025-12-01 10:51:40 +0400

    quiz commit

---
+++ python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""It is doc string"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    response = requests.get(url)
+
+    if response.status_code >= 400:
+        print(f"Error code: {response.status_code}")
+    else:
+        print(response.text)
