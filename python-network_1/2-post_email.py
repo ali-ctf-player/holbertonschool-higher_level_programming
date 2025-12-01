464044720e2fa37ddb0c8d1f0c87112ef2c53dd9
ali-ctf-player <eliekberbabayev23082007@gmail.com>
2025-12-01 10:06:04 +0400

    quiz commit

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -9,13 +9,12 @@ if __name__ == "__main__":
 
     url = sys.argv[1]
     email = sys.argv[2]
-    values = {"email":email}
+    values = {"email": email}
 
     data = urllib.parse.urlencode(values).encode("utf-8")
-    req = urllib.request.Request(url,data=data,method="POST")
+    req = urllib.request.Request(url, data=data, method="POST")
 
     with urllib.request.urlopen(req) as response:
         the_page = response.read().decode("utf-8")
-    
-        print(the_page)
 
+        print(the_page)
