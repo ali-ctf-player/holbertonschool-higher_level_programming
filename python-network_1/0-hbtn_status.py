352b9e2e7d40d7769017b9f89d56e36c1aaadb33
ali-ctf-player <eliekberbabayev23082007@gmail.com>
2025-12-01 09:31:01 +0400

    quiz commit

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import urllib.request
+
+req = urllib.request.Request('https://intranet.hbtn.io/status')
+
+with urllib.request.urlopen(req) as response:
+    the_page = response.read()
+
+print(f"\t- type: {type(the_page)}")
+print(f"\t- content: {the_page}")
+print(f"\t- utf8 content: {the_page.decode('utf-8')}")
