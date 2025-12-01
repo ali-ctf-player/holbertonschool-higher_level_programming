1619d503738571088966703807130fd77aeeace5
ali-ctf-player <eliekberbabayev23082007@gmail.com>
2025-12-01 10:48:22 +0400

    quiz commit

---
+++ python-network_1/6-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""It is doc string"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    data = {"email": email}
+
+    response = requests.post(url, data=data)
+
+    print(response.text)
